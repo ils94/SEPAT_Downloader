263232cb62aa562c17ba5da3b0ff206762be8f17
None <None>
2023-01-09 13:03:56 -0300

    adicionado uma maneira de limpar os downloads dentro do proprio app.

--- app/src/main/java/com/droidev/sepatdownloader/MainActivity.java
+++ app/src/main/java/com/droidev/sepatdownloader/MainActivity.java
@@ -3,23 +3,26 @@ package com.droidev.sepatdownloader;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.ActivityManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
     Button sepatLevantamento, sepatScanner, sepatInventario, sepatAgenda;
     TextView link;
-    Downloads downloads;
+    private Downloads downloads;
 
-    String sepatLevantamentoURL = "https://github.com/ils94/SEPAT_Levantamento/releases/download/release/sepat-levantamento.apk";
-    String sepatScannerURL = "https://github.com/ils94/SEPAT_Scanner/releases/download/release/sepat-scanner.apk";
-    String sepatAgendaURL = "https://github.com/ils94/SEPAT_Agenda/releases/download/release/sepat-agenda.apk";
-    String sepatInventarioURL = "https://github.com/ils94/SEPAT_Inventario/releases/download/release/sepat-inventario.apk";
+    private final String sepatLevantamentoURL = "https://github.com/ils94/SEPAT_Levantamento/releases/download/release/sepat-levantamento.apk";
+    private final String sepatScannerURL = "https://github.com/ils94/SEPAT_Scanner/releases/download/release/sepat-scanner.apk";
+    private final String sepatAgendaURL = "https://github.com/ils94/SEPAT_Agenda/releases/download/release/sepat-agenda.apk";
+    private final String sepatInventarioURL = "https://github.com/ils94/SEPAT_Inventario/releases/download/release/sepat-inventario.apk";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,8 +95,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void downloadIniciado() {
+    private void downloadIniciado() {
 
         Toast.makeText(MainActivity.this, "Download iniciado, aguarde.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.limparDownloads) {
+
+            clearData();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void clearData() {
+
+        try {
+            if (Build.VERSION_CODES.KITKAT <= Build.VERSION.SDK_INT) {
+                ((ActivityManager) getSystemService(ACTIVITY_SERVICE)).clearApplicationUserData();
+            } else {
+
+                Runtime.getRuntime().exec("pm clear " + getApplicationContext().getPackageName());
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, String.valueOf(e), Toast.LENGTH_SHORT).show();
+        }
     }
 }
